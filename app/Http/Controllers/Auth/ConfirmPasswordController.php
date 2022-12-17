e0a0a31524831e65cb5ee0ddfc8f9153dc057fc3
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2022-12-17 17:56:54 +0100

    Update ConfirmPasswordController.php

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\ConfirmsPasswords;
 
 class ConfirmPasswordController extends Controller
 {
+
     /*
     |--------------------------------------------------------------------------
     | Confirm Password Controller
